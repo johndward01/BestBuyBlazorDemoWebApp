55cd644327259091ebf30762271559086beea9dc
johndward <johndward01@gmail.com>
2022-04-07 14:46:10 -0500

    Add project files.

---
+++ BestBuyBlazorDemoWebApp/Data/ProductRepo.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace BestBuyBlazorDemoWebApp.Data
+{
+    public class ProductRepo
+    {
+        private readonly IConfiguration _config;
+        public ProductRepo(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            using IDbConnection db = new MySqlConnection(_config.GetConnectionString("bestbuy"));
+            db.Open();
+            var products = db.Query<Product>("SELECT * FROM products;");
+            return products;
+        }
+    }
+}
